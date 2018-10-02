d04493e5074e85694dc5ca3c3d95fc40aa787260
Frank Aceves <frankaceves@gmail.com>
2018-10-01 19:12:18 -0700

    add DogClient file to Model

---
+++ dogFinder/Model/DogClient.swift
@@ -0,0 +1,13 @@
+//
+//  DogClient.swift
+//  dogFinder
+//
+//  Created by Frank Aceves on 10/1/18.
+//  Copyright © 2018 Frank Aceves. All rights reserved.
+//
+
+import Foundation
+
+class DogClient: NSObject {
+    
+}
