13b7207ec1dc3d8984adb6c04a0f4146f8720447
Frank Aceves <frankaceves@gmail.com>
2018-10-01 19:30:35 -0700

    add DogConstants file + add structs & download methods

---
+++ dogFinder/Model/DogConstants.swift
@@ -0,0 +1,37 @@
+//
+//  DogConstants.swift
+//  dogFinder
+//
+//  Created by Frank Aceves on 10/1/18.
+//  Copyright © 2018 Frank Aceves. All rights reserved.
+//
+
+extension DogClient {
+    struct Constants {
+        struct RandomDog: Decodable {
+            let status: String
+            let message: String
+        }
+        
+        struct AllDogBreeds: Decodable {
+            let status: String
+            //let message: [Breed: [SubBreed]] // [breed: [subBreed]]
+            let message: [String: [String]]
+        }
+        
+        struct Breed: Decodable {
+            let breed: String
+        }
+        
+        struct SubBreed: Decodable {
+            let subBreed: [String]
+        }
+        
+        
+        //RANDOM DOG JSON
+        struct APIUrls {
+            static let randomDogAPIString = "https://dog.ceo/api/breeds/image/random"
+        }
+        
+    }
+}
