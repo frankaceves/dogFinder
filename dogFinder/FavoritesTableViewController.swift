39dd89d2fb26b5a36dcf59b110ddb69663a8458f
Frank Aceves <frankaceves@gmail.com>
2018-10-20 20:56:24 -0700

    uncomment canEditRowAt in FavoritesTableVC

--- dogFinder/FavoritesTableViewController.swift
+++ dogFinder/FavoritesTableViewController.swift
@@ -150,13 +150,13 @@ class FavoritesTableViewController: UITableViewController {
     }
     
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+ 
 
     /*
     // Override to support editing the table view.
