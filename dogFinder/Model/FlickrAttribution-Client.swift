12515907410bf0014ab93effffd7a58639f8c6d4
Frank Aceves <frankaceves@gmail.com>
2019-03-20 08:31:14 -0700

    add Flickr owner structs for ID purposes to FlickrAttribution-Client.swift file

--- dogFinder/Model/FlickrAttribution-Client.swift
+++ dogFinder/Model/FlickrAttribution-Client.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+struct Result: Codable {
+    let stat: String
+    let photo: PhotoInfo
+}
+
+struct PhotoInfo: Codable  {
+    let id: String
+    let urls: Urls
+    let owner: Owner
+}
+
+struct Owner: Codable {
+    let username: String
+}
+
+struct Urls: Codable  {
+    let url: [Url]
+}
+
+struct Url: Codable  {
+    let type: String
+    let _content: String
+}
