6e0754832b754ee1d00cf51a72a2edb1d41b217c
Frank Aceves <frankaceves@gmail.com>
2019-03-20 21:06:41 -0700

    add Dog-CoreDataInfo.swift file and properties

---
+++ dogFinder/Model/Dog-CoreDataInfo.swift
@@ -0,0 +1,17 @@
+//
+//  Dog-CoreDataInfo.swift
+//  dogFinder
+//
+//  Created by Frank Aceves on 3/20/19.
+//  Copyright © 2019 Frank Aceves. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+struct Dog {
+    var imageURL: String!
+    var imageData: Data!
+    var id: String!
+    var attribution: String!
+}
