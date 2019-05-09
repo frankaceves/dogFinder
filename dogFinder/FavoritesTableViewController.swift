d5be75336df392c541636d4592826b9060656969
Frank Aceves <frankaceves@gmail.com>
2019-05-08 19:57:06 -0700

    remove comments in numberOfRowsInSection

--- dogFinder/FavoritesTableViewController.swift
+++ dogFinder/FavoritesTableViewController.swift
@@ -77,7 +77,6 @@ class FavoritesTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return fetchedResultsController.fetchedObjects?.count ?? 0
     }
 
