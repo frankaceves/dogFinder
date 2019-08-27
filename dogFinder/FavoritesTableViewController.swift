1b5eb64929f38ee919baac948048cf5bbaddc266
Frank Aceves <frankaceves@gmail.com>
2019-08-27 11:19:39 -0700

    add isFiltered property to FavoritestableVC

--- dogFinder/FavoritesTableViewController.swift
+++ dogFinder/FavoritesTableViewController.swift
@@ -12,6 +12,7 @@ import CoreData
 class FavoritesTableViewController: UITableViewController {
     var fetchedResultsController: NSFetchedResultsController<FavoriteDog>!
     var dataController: DataController!
+    var isFiltered: Bool = false //initial state of app is non-filtered state.
     
     override func viewDidLoad() {
         super.viewDidLoad()
