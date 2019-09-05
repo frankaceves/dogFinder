e10dd491348e0d3f3f5adaa35cf01995eacb7d29
Frank Aceves <frankaceves@gmail.com>
2019-09-05 11:04:25 -0700

    import reachability

--- dogFinder/FavoritesTableViewController.swift
+++ dogFinder/FavoritesTableViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import Reachability
 
 class FavoritesTableViewController: UITableViewController {
     var fetchedResultsController: NSFetchedResultsController<FavoriteDog>!
--- dogFinder/RandomDogViewController.swift
+++ dogFinder/RandomDogViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import Reachability
 
 class RandomDogViewController: UIViewController {
     
